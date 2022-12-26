93c0bec42b0331caeae6fbe918cd4caf65220716
Md-Anisuzzamane <anisuzzaman1199@gmail.com>
2022-12-27 01:23:05 +0600

    redux-toolkit add

--- src/index.js
+++ src/index.js
@@ -4,13 +4,17 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from "react-router-dom";
+import { provider } from "react-redux";
+import { store } from './App/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <provider>
+      <BrowserRouter store={store} >
+        <App />
+      </BrowserRouter>
+    </provider>
   </React.StrictMode>
 );
 
